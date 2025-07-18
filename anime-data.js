a28677912be1e7c3114926137ad50b651c50270e
anime-bkcc <asdliks88ss@gmail.com>
2025-07-19 02:02:03 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -6,14 +6,14 @@ const animeList = [
     img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",
     type: "ตอนที่ 1-169 ซับไทย.",
     link: "https://shen-yin-wangzuo.github.io/"
-      }
+      },
         {
   
     title: "Shen Yin Wangzuo ผนึกเทพบัลลังก์ราชันย์ ตอนที่ 1-6 พากย์ไทย",
     img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",
     type: "ตอนที่ 1-6 พากย์ไทย.",
     link: "https://shen-yin-wangzuo.github.io/thai"
-}
+},
 
       {
   
@@ -21,7 +21,7 @@ const animeList = [
     img: "https://img2.pic.in.th/pic/Gachiakuta36af1c3c5aa00cfb.jpg",
     type: "ตอนที่ 1-3 พากย์ไทย",
     link: "https://gachiakuta-thai.github.io/"
-      }
+      },
 
 {
   
@@ -29,6 +29,6 @@ const animeList = [
     img: "https://img2.pic.in.th/pic/Gachiakuta36af1c3c5aa00cfb.jpg",
     type: "ตอนที่ 1-3 ซับไทย",
     link: "https://gachiakuta-thai.github.io/sub"
-}
+},
   // เพิ่มรายการอนิเมะได้เรื่อย ๆ
 ];
